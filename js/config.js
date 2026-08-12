357771ec295c71b25874b37fec2628d1a8c7ec31
tiamobew <tiamobew@gmail.com>
2026-08-12 09:28:29 +0700

    separate games into skyisland and Adventure

--- js/config.js
+++
@@ -1,7 +0,0 @@
-/*
- * ตั้งค่าครั้งเดียวหลัง Deploy Google Apps Script เป็น Web app
- * ตัวอย่าง: webAppUrl: 'https://script.google.com/macros/s/XXXXXXXX/exec'
- */
-window.SKYISLAND_CONFIG = {
-  webAppUrl: 'https://script.google.com/macros/s/AKfycbxlW1rkljAUd6v78jows6vaUZYSWNdnGWaYmBZlmSkIVfABR1pH_GgeOYP8R67mKIB0eA/exec'
-};
